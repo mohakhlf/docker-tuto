f99bf1f3b704ad6e293e2909b47227f2707b0c60
mohakhlf <mohakhellafi@gmail.com>
2020-08-15 14:30:11 +0200

    apprendre à dockerriser un projet

---
+++ index.js
@@ -0,0 +1,11 @@
+let express = require('express')
+
+let app = express()
+
+app.get('/', function (req,res) {
+    res.send('Hello World!')
+})
+
+app.listen(8081, function () {
+    console.log('app listening on port 8081')
+})
